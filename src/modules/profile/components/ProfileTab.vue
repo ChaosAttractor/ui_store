17675de1b0c3efaf65fbe2df24a51ed8e652334a
Данила Стебаков <danila.stebakov@areal.company>
2024-02-08 23:05:21 +0300

    feat: added divider

--- src/modules/profile/components/ProfileTab.vue
+++ src/modules/profile/components/ProfileTab.vue
@@ -1,18 +1,15 @@
 <template>
   <section class="profile-tab">
     <section class="profile-tab__sections">
-      <section>
-        <h3>Профиль</h3>
-        <section class="profile-tab__form">
-          <KusakabeInput :value="profile.lastName" label="Фамилия" placeholder="Фамилия" />
-          <KusakabeInput :value="profile.firstName" label="Имя" placeholder="Имя" />
-          <KusakabeInput :value="profile.email" label="Почта" placeholder="Почта" />
-        </section>
+      <h3>Профиль</h3>
+      <section class="profile-tab__form">
+        <KusakabeInput :value="profile.lastName" label="Фамилия" placeholder="Фамилия" />
+        <KusakabeInput :value="profile.firstName" label="Имя" placeholder="Имя" />
+        <KusakabeInput :value="profile.email" label="Почта" placeholder="Почта" />
       </section>
-      <section>
-        <h3>Безопасность</h3>
-        <el-button> Сменить пароль </el-button>
-      </section>
+      <el-divider />
+      <h3>Безопасность</h3>
+      <el-button> Сменить пароль </el-button>
     </section>
   </section>
 </template>
@@ -34,9 +31,9 @@ export default {
 <style lang="sass" scoped>
 .profile-tab
   &__sections
-    display: flex
-    flex-direction: column
-    row-gap: 10px
+    //display: flex
+    //flex-direction: column
+    //row-gap: 10px
   &__form
     display: flex
     flex-direction: column
